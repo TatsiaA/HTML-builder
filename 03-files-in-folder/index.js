a6bb466146ca2a7214f747d25e09990a9baef6dc
TatsiaA <taniokk@mail.ru>
2021-11-07 23:42:37 +0300

    feat: add solution 3 (size)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,36 @@
-const secretFolder = './03-files-in-folder/secret-folder';
 const fs = require('fs');
 const path = require('path');
+const secretFolder = path.join(__dirname, 'secret-folder');
+
+let stats = fs.stat(path.join(__dirname, 'secret-folder', 'text.txt'), (err, stats) => {
+     if (err) throw err;
+     console.log(stats.isFile());
+     console.log(stats.size);
+   });
+   //let fileSizeInBytes = stats.size;
+   
+   
 fs.readdir(secretFolder, {withFileTypes: true}, (err, files) => {
   if (err) throw err;  
   files.forEach(file => {
     if (file.isFile()) {
-        
-    fs.stat(secretFolder, file, (err, stats)=> {
       
-     /*   let a = path.parse(file.name).ext.slice(1);        
+    fs.stat(path.join(secretFolder, file.name), (err, stats)=> {
+      if (err) throw err;
+      //console.log(file);  
+        let a = path.parse(file.name).ext.slice(1);        
         let name = path.parse(file.name).name;
-        let size = file.size;
-        console.log(`${name} - ${a} - ${size}`);  */
-        let size1 = stats.size;
+        let size = stats.size / 1024;
+        console.log(`${name} - ${a} - ${size} kb`);  
+       /* let size1 = stats.size;
+        
         let ext = path.extname(file.name);
         let basename = path.basename(file.name, ext);
         console.log(`${basename} - ${ext.slice(1)} - ${size1}`);
-        //console.log(stats);
-        //let fullName = {file, stats};
-        //console.log(fullName);
+        
+        */
     })
     };
   });
 });
+
