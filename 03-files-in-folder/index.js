e49a32b9adc3a682361ec3bd4111a17ae2733b48
TatsiaA <taniokk@mail.ru>
2021-11-07 23:43:39 +0300

    fix: delete test code

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,13 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const secretFolder = path.join(__dirname, 'secret-folder');
 
-let stats = fs.stat(path.join(__dirname, 'secret-folder', 'text.txt'), (err, stats) => {
-     if (err) throw err;
-     console.log(stats.isFile());
-     console.log(stats.size);
-   });
-   //let fileSizeInBytes = stats.size;
-   
    
 fs.readdir(secretFolder, {withFileTypes: true}, (err, files) => {
   if (err) throw err;  
